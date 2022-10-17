70203a656edfe4b16e2fe873ce09ba7796bd1b62
JoseLuisHcoder <zonajl@gmail.com>
2022-10-17 07:39:25 -0400

    upload crud react native

---
+++ reactNative/crudReactNative/crudrn/src/config/fb.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore';
+import Constants  from "expo-constants";
+
+const firebaseConfig = {
+  apiKey: Constants.manifest.extra.apiKey,
+  authDomain: Constants.manifest.extra.authDomain,
+  projectId: Constants.manifest.extra.projectId,
+  storageBucket: Constants.manifest.storageBucket,
+  messagingSenderId: Constants.manifest.extra.messagingSenderId,
+  appId: Constants.manifest.extra.appId,
+  databaseURL: Constants.manifest.extra.databaseURL
+};
+
+
+initializeApp(firebaseConfig);
+export const database = getFirestore();
